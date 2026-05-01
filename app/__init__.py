29ef55ec386483d6ebad2bf5d095b196f96e9737
ericachinlk <88417227+ericachinlk@users.noreply.github.com>
2026-05-01 14:42:11 +0800

    Remove unused declaration

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,4 @@ from app.config import read_config, set_algorithm, ConfigError, toggle_perfect
 from app.generate_maze import generate_maze
 
 __all__ = ["read_config", "generate_maze", "set_algorithm",
-           "ConfigError", "toggle_perfect", "AppRenderer"]
+           "ConfigError", "toggle_perfect"]
